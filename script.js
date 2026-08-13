49c4f09f774cefff893e9a66904e4a296cb75b85
Ilenia Tundo <ilenia.tundo@yahoo.de>
2026-08-13 16:42:28 +0200

    Add spaceship controls and status messages

--- script.js
+++ script.js
@@ -1,115 +1,144 @@
-let spaceshipName = "Nova"
+const spaceshipName = "Nova"
 let shipHealth = 100;
 let starCoins = 5;
-let inventory = ["repair kit", " repair kit"]
+let inventory = 2;
 
 
-function getStatus() {
-    console.log("Spaceship Name: " + spaceshipName);
-    console.log("Life: " + shipHealth);
-    console.log("Star Coins: " + starCoins);
-    console.log("Inventory: " + inventory);
+function renderStatus() {
+
+    document.getElementById('renderStatus').innerHTML = `
+        <h2> SPACESHIP STATUS </h2>
+        <table>
+            <tr>
+                <td>Spaceship</td>
+                <td>${spaceshipName}</td>
+            </tr>
+            <tr>
+                <td>Star Coins</td>
+                <td>${starCoins}</td>
+            </tr>
+            <tr>
+                <td>Health</td>
+                <td>${shipHealth}</td>
+            </tr>
+            <tr>
+                <td>Inventory</td>
+                <td>${inventory}</td>
+            </tr>
+        </table>`
 }
 
+function showError(message) {
+    document.getElementById("errorMessage").innerHTML = message;
+    document.getElementById("infoMessage").innerHTML = "";
+}
+
+function showInfo(message) {
+    document.getElementById("infoMessage").innerHTML = message;
+    document.getElementById("errorMessage").innerHTML = "";
+}
 
 function useRepairKit() {
-    if (shipHealth < 100 && inventory.length > 0) {
-        inventory.pop();
+
+    if (shipHealth < 100 && inventory > 0) {
+        inventory = inventory - 1;
         shipHealth = shipHealth + 50;
         if (shipHealth > 100) {
             shipHealth = 100;
         }
-        console.log(
-            "The spaceship was repaired. + 50 health! Health is now " + shipHealth,
-        );
-    } else if (inventory.length <= 0) {
-        console.log("No repair kits left.");
+        showInfo("The spaceship was repaired.");
+
+    } else if (inventory <= 0) {
+        showError("No repair kits left!");
 
     } else if (shipHealth === 100) {
-        console.log("Health is already at 100");
+        showError("Health is already at 100%.");
     }
+
+    renderStatus();
 }
 
 
 
 function buyRepairKit() {
-    if (starCoins > 0) {
-        starCoins = starCoins - 1;
-        inventory.push("Repair-Kit");
-        console.log("Repair-Kit gekauft!");
+
+    let repairKitAmount = Number(document.getElementById("repairInput").value);
+
+    // Wenn Repair-Kit-Anzahl größer 0 UND die Repair-Kit-Anzahl kleiner/gleich StarCoins → Kauf durchführen.
+    if (repairKitAmount > 0 && repairKitAmount <= starCoins) {
+        starCoins = starCoins - repairKitAmount;
+
+        inventory = inventory + repairKitAmount;
+
+        showInfo("Repair kit purchased!");
+
+    } else if (repairKitAmount === 0) {
+        // nichts bzw. 0 eingegeben
+        showError("Please enter an amount!");
     }
     else {
-        console.log("Leider nicht genug Star Coins!");
+        // console.log("Leider nicht genug Star Coins!");
+        // document.getElementById("buyMessage").innerHTML = "You don't have enough Star Coins!";
+
+        showError("You don't have enough Star Coins!");
+
     }
+
+    // den Inputfeld nach der Eingabe leeren
+    document.getElementById("repairInput").value = "";
+
+    // Status aufrufen
+    renderStatus();
+
 }
 
 
 
-function takeDamage() {
+function getDamage() {
+    // ------------- DAMAGE THE SHIP RANDOM ------------- 
     // Return a random integer from 1 to 99 (both included):
-    let damage = Math.floor(Math.random() * 100) + 1;
+    // let damage = Math.floor(Math.random() * 100) + 1;
 
-    shipHealth = shipHealth - damage;
+    // shipHealth = shipHealth - damage;
+
+    // if (shipHealth > 0) {
+    //     console.log("You took " + damage + " damage");
+    //     console.log("Your health is " + shipHealth);
+    // }
+
+    // if (shipHealth <= 0) {
+    //     console.log("Your ship was destroyed. Game over!");
+
+    //     document.getElementById().innerHTML = 'Game Over!'
+    // }
+    // ================================================================
+
+
+    // ------------- DAMAGE THE SHIP with Input field ------------- 
+    let damage = Number(document.getElementById('damageInput').value);
 
     if (shipHealth > 0) {
-        console.log("You took " + damage + " damage");
-        console.log("Your health is " + shipHealth);
-        if (shipHealth === 0) {
-            console.log("Your ship was destroyed. Game over!")
-        }
-    } else {
-        shipHealth = 0
-        console.log("Your ship was destroyed. Game over!")
+        shipHealth = shipHealth - damage;
     }
 
+    if (shipHealth <= 0) {
+        shipHealth = 0;
+        showError("Your ship is destroyed. Game Over!");
+    } else {
+        showInfo("You took " + damage + " damage.");
+    }
+
+    // den Inputfeld nach der Eingabe leeren
+    document.getElementById("damageInput").value = "";
+
+    // Status nach dem Damage aufrufen
+    renderStatus();
 }
 
 
+renderStatus();
+// getDamage();
+// useRepairKit();
+// buyRepairKit()
+// renderStatus();
 
-getStatus();
-takeDamage();
-useRepairKit();
-buyRepairKit()
-getStatus();
-
-getStatus();
-takeDamage();
-useRepairKit();
-buyRepairKit()
-getStatus();
-
-getStatus();
-takeDamage();
-useRepairKit();
-buyRepairKit()
-getStatus();
-
-getStatus();
-takeDamage();
-useRepairKit();
-buyRepairKit()
-getStatus();
-
-getStatus();
-takeDamage();
-useRepairKit();
-buyRepairKit()
-getStatus();
-
-getStatus();
-takeDamage();
-useRepairKit();
-buyRepairKit()
-getStatus();
-
-getStatus();
-takeDamage();
-useRepairKit();
-buyRepairKit()
-getStatus();
-
-getStatus();
-takeDamage();
-useRepairKit();
-buyRepairKit()
-getStatus();
